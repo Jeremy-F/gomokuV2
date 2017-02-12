f82246c7b0fb1e58b91c6a1a701912fbac16c942
Mireille Fornarino <blay@unice.fr>
2017-02-12 16:55:02 +0100

    cleaning IA

--- src/main/java/fr/esiee/player/IA.java
+++ src/main/java/fr/esiee/player/IA.java
@@ -24,42 +24,12 @@ public class IA extends Player{
         super(name, color);
     }
 
-    /*public int eval(){
-        if(this.getBoard().isFinished()){
-            if(this.haveWin()){
-                return 1000;
-            }else if(this.wonByOtherPlayer()){
-                return -100;
-            }else{
-                return 0;
-            }
-        }
-        return -3;
-    }*/
 
     @Override
     public boolean play(Board board) {
 
         return Strategy.playRandom(board, this);
-
-/*        //It should not play if the game is up
-        if (board.isFinished()) {
-            //todo : remove
-            System.out.println("game is over ! ");
-            return false;
-        }
-
-        int randomLine   = (int)(Math.random() * board.dimension());
-        int randomColumn = (int)(Math.random() * board.dimension());
-        if(board.getBox(randomLine, randomColumn).hasOwner()){
-            return play(board);
-        }else {
-            board.play(randomLine, randomColumn);
-        }
-        return true;*/
     }
-
-
 
 
 
